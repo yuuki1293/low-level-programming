d4c4b406d3695d70e8df0305b38bd35e29a17794
yuuki1293 <yuuki1293@outlook.jp>
2023-02-24 21:50:34 +0900

    リスト2-11

--- endianness.asm
+++ endianness.asm
@@ -2,6 +2,8 @@ section .data
 
 newline_char: db 10
 codes: db '0123456789abcdef'
+demo1: dq 0x1122334455667788
+demo2: db 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88
 
 section .text
 global _start
@@ -39,7 +41,11 @@ iterate:
 
     ret
 _start:
-    mov rdi, 0x1122334455667788
+    mov rdi, [demo1]
+    call print_hex
+    call print_newline
+
+    mov rdi, [demo2]
     call print_hex
     call print_newline
 
