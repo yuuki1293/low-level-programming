4d1b0afc0a71a148b4ec7045b281162e8b4ffb05
yuuki1293 <yuuki1293@outlook.jp>
2023-04-10 09:30:30 +0900

    _start関数

---
+++ main.asm
@@ -0,0 +1,47 @@
+section .text
+%include "colon.inc"
+%include "words.inc"
+
+extern find_word
+extern read_word
+extern print_error
+extern print_string
+extern string_length
+extern exit
+
+global _start
+
+section .rodata
+msg_not_found: db "Not found", 10, 0
+
+section .text
+_start:
+    mov rdi, rsp
+    sub rsp, 256
+    mov rsi, 255
+    call read_word
+    mov rdi, rax
+    mov rsi, lw
+    call find_word
+    test rax, rax
+    jz .error
+
+    add rax, 8  ; lwがdq(8byte)なので8進める
+    mov rdi, rax
+    push rdi
+    call string_length
+    pop rdi
+    add rdi, rax
+    inc rdi     ; null文字の分進める
+    call print_string
+
+    add rsp, 256
+    mov rdi, 0
+    call exit
+.error:
+    mov rdi, msg_not_found
+    call print_error
+
+    add rsp, 256
+    mov rdi, 0
+    call exit
