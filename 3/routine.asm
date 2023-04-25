93910a22102bd2971ac2312b76d62d03ceb20905
yuuki1293 <yuuki1293@outlook.jp>
2023-04-25 19:44:55 +0900

    定数定義とスタックを確保

--- 3/routine.asm
+++ 3/routine.asm
@@ -2,6 +2,17 @@ section .text
 
 export string_equals
 
+%define pc r15
+%define w r14
+%define rstack r13
+
+section .bss
+resq 1023
+rstack_start: resq 1
+input_buf: resb 1024
+
+section .text
+
 ; ヌルで終わる文字列へのポインタを受け取り、そのワードヘッダの開始アドレスを返す。もし、その名前のワードがなければ、ゼロを返す。
 ; rdi 文字列
 ; rax 開始アドレス
@@ -29,3 +40,16 @@ native "find_word" find_word
 ; ワードヘッダの開始アドレスを受け取り、実行トークン（XT）の値に到達するまでヘッダ全体をスキップする。
 ; rdi ワードヘッダの開始アドレス
 native "cfa", cfa
+    pop rsi
+    add rsi, 9
+    .loop:
+    mov al, [rsi]
+    test al, al
+    jz .end
+    inc rsi
+    jmp .loop
+
+    .end:
+    add rsi, 2
+    push rsi
+    jmp next
