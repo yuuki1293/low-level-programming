7c5f7337b376d8dd7eb64de490d35a3c8283bd4e
yuuki1293 <yuuki1293@outlook.jp>
2023-04-25 20:46:23 +0900

    入力用バッファを確保

--- 3/routine.asm
+++ 3/routine.asm
@@ -2,27 +2,17 @@ section .text
 
 export string_equals
 
+section .bss
+
+input_buf: resb 1024
+
+section .text
+
 ; ヌルで終わる文字列へのポインタを受け取り、そのワードヘッダの開始アドレスを返す。もし、その名前のワードがなければ、ゼロを返す。
 native "find_word" find_word
-    xor rax, rax
-    mov rsi, last_word
+    mov rsi, [last_word]
 .loop:
-    test rsi, rsi
-    jz .end
-    push rsi
-    add rsi, 8
-    push rdi
-    call string_equals
-    pop rdi
-    pop rsi
-    test rax, rax
-    jnz .found
-    mov rsi, [rsi]
-    jmp .loop
-.found:
-    mov rax, rsi
-.end:
-    ret
+    mov rdi, [rsp]
 
 ; ワードヘッダの開始アドレスを受け取り、実行トークン（XT）の値に到達するまでヘッダ全体をスキップする。
 native "cfa", cfa
