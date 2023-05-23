d941b95c9ac5c584920b70cd8759b893d7d69716
yuuki1293 <yuuki1293@outlook.jp>
2023-05-23 13:03:45 +0900

    list.hを作成

---
+++ 10/list.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#ifndef _LIST_C_
+#define _LIST_C_
+
+struct node
+{
+    list p;
+    int num;
+};
+
+typedef struct node *list;
+
+list list_create(int);
+
+void list_add_front(int, list *);
+
+void list_add_back(int, list *);
+
+int list_get(size_t, const list);
+
+void list_free(list);
+
+int list_length(list);
+
+int list_sum(list);
+
+#endif
