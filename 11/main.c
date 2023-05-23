21058e262ca7dc83cdfec3e211053b7dded9bc8f
yuuki1293 <yuuki1293@outlook.jp>
2023-05-23 22:07:48 +0900

    scan_listを作成

--- 11/main.c
+++ 11/main.c
@@ -3,6 +3,15 @@
 #include "list.h"
 #include "scalar_product.h"
 
+void scan_list(struct list *list)
+{
+    int value;
+    while (scanf("%d", &value) != EOF)
+    {
+        list_add_front(value, list);
+    }
+}
+
 int main(int argc, char **argv)
 {
     return 0;
