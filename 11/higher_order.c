57056c87ca927c99e6c25085961f569751e74310
yuuki1293 <yuuki1293@outlook.jp>
2023-05-24 10:35:55 +0900

    mapを実装

--- 11/higher_order.c
+++ 11/higher_order.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include "list.h"
 
-void foreach(struct list* list, void (*f)(int)){
+void foreach (struct list *list, void (*f)(int))
+{
     while (list != NULL)
     {
         f((*list).value);
         list = (*list).next;
     }
+}
+
+struct list *map(const struct list *list, int (*f)(int))
+{
+    struct list *new_list;
+    struct list *new_node;
+
+    if (!list)
+        return NULL;
+
+    new_list = list_create(f((*list).value));
+    new_node = new_list;
+    list = (*list).next;
+    while (list != NULL)
+    {
+        (*new_node).next = list_create(f((*list).value));
+        list = (*list).next;
+    }
+    return new_list;
 }
--- 11/higher_order.h
+++ 11/higher_order.h
@@ -3,4 +3,6 @@
 
 void foreach (struct list *, void (*)(int));
 
+struct list *map(const struct list *, int (*)(int));
+
 #endif
