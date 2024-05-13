6f304e63fb757216e48a53bf03d922cb50b396f3
yuuki1293 <yuuki1293@outlook.jp>
2024-05-13 11:18:48 +0900

    bmp.cを修正

--- 16/bmp.c
+++ 16/bmp.c
@@ -10,7 +10,12 @@ size_t load_bmp(const char* filename, struct bmp_header* header, struct image* i
         fclose(fp);
         return 1;
     }
-    if (fread(image, sizeof(struct image), 1, fp) < 1) {
+
+    image->height = header->biHeight;
+    image->width = header->biWidth;
+    image->array = calloc(header->biHeight * header->biWidth, sizeof(struct pixel));
+
+    if (fread(image->array, sizeof(struct pixel), image->height * image->width, fp) < 1) {
         fclose(fp);
         return 1;
     }
@@ -19,11 +24,11 @@ size_t load_bmp(const char* filename, struct bmp_header* header, struct image* i
 
 size_t save_bmp(const char* filename, struct bmp_header* header, struct image* image) {
     FILE* fp = fopen(filename, "wb");
-    if (fwrite(header, sizeof(struct bmp_header), 1, fp)) {
+    if (!fwrite(header, sizeof(struct bmp_header), 1, fp)) {
         fclose(fp);
         return 1;
     }
-    if (fwrite(image->array, sizeof(struct pixel), header->biWidth * header->biHeight, fp)) {
+    if (!fwrite(image->array, sizeof(struct pixel), header->biWidth * header->biHeight, fp)) {
         fclose(fp);
         return 1;
     }
