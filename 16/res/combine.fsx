f06f577ca0524e740a50c78d17ea563084dede06
yuuki1293 <yuuki1293@outlook.jp>
2024-05-17 18:27:51 +0900

    combine.fsxを更新

--- 16/res/combine.fsx
+++ 16/res/combine.fsx
@@ -4,17 +4,12 @@ open System
 let path = "./"
 let out = "combine.csv"
 
-let files = Directory.GetFiles(path, "*.csv") |> Array.where (fun x -> Path.GetFullPath out <> Path.GetFullPath x)
-let data = [ for fName in files do yield File.ReadAllLines fName |> Array.toList ]
-
-seq {
-    yield files |> Array.toSeq |> Seq.map Path.GetFileNameWithoutExtension
-    for i in 0..data[0].Length-1 do
-        yield seq {
-            for d in data do
-                yield d[i]
-        }
-}
-|> Seq.map (fun x -> String.Join(",", x))
-|> Seq.toList
+Directory.GetFiles(path, "*.csv")
+|> Array.where (fun x -> Path.GetFullPath out <> Path.GetFullPath x)
+|> fun files ->
+files
+|> Array.map File.ReadAllLines
+|> Array.transpose
+|> Array.append [| files |> Array.map Path.GetFileNameWithoutExtension |]
+|> Array.map (String.concat ",")
 |> fun x -> File.WriteAllLines(out, x)
